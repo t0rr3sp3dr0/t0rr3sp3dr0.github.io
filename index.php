536970ad9f98270aedcfa696187ffb3951fcf237
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2017-02-28 20:59:59 -0300

    CC 2016.1

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-//if ($_SERVER['HTTPS'] != 'on') {
-//    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-//    exit();
-//} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== false) {
-//    header('Location: https://gist.githubusercontent.com/t0rr3sp3dr0/af2e4eafd721d365c7bb6db09919eff9/raw/CInstala.sh');
-//    exit();
-//}
+if ($_SERVER['HTTPS'] != 'on') {
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== false) {
+    header('Location: https://gist.githubusercontent.com/t0rr3sp3dr0/af2e4eafd721d365c7bb6db09919eff9/raw/CInstala.sh');
+    exit();
+}
 
 readfile('index.html');
